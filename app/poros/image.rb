64c092cdd6c837992dbe7616f6ec366cffa4df32
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-24 23:11:44 -0400

    Add image poro and test

--- app/poros/image.rb
+++ app/poros/image.rb
@@ -2,9 +2,9 @@ class Image
   attr_reader :url, :description, :source_name, :source_url
   def initialize(data)
     image = data[:results][0]
-    credit = data[:user]
+    credit = image[:user]
     @url = image[:urls][:full]
-    @description = data[:alt_description]
+    @description = image[:alt_description]
     @source_name = credit[:name]
     @source_url = credit[:links][:html]
     #unsplash url?
---
+++ spec/poros/image_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Image, type: :poro do
+  before do
+    response = File.read("spec/fixtures/images/good_response.json")
+    body = JSON.parse(response, symbolize_names: true)
+
+    @image = Image.new(body)
+  end
+
+  it "has attributes and source data" do
+    expect(@image.url).to eq("https://images.unsplash.com/photo-1602276506752-cec706667215?crop=entropy&cs=srgb&fm=jpg&ixid=MnwzMjIxNjF8MHwxfHNlYXJjaHwxfHxjaGljYWdvJTJDJTIwaWx8ZW58MHx8fHwxNjUwNzU3Njc2&ixlib=rb-1.2.1&q=85")
+    expect(@image.description).to eq("white concrete building during daytime")
+
+    expect(@image.source_name).to eq("Dylan LaPierre")
+    expect(@image.source_url).to eq("https://unsplash.com/@drench777")
+  end
+end
