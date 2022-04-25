229de3d230b2fc4494e3ab8dba4a178e3358b4f2
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-24 22:16:51 -0400

    second test pass third in progress

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe ImageService, type: :service do
     expected = [base, key, options, query].join
     expect(ImageService.build_url('atlanta, ga')).to eq expected
   end
+
+  it "recieves a 200 response" do
+    base = 'https://api.unsplash.com/search/photos?'
+    key = "client_id=#{ENV['unsplash_api']}&"
+    options = 'order_by=relevant&page=1&per_page=1&'
+    query = "query=atlanta, ga"
+
+    url = [base, key, options, query].join
+
+    response = ImageService.send_request(url)
+
+    expect(response.status).to eq 200
+  end
+
+  it "returns the data for one image" do
+    response = ImageService.get_image('atlanta, ga')
+
+   expect(response[:total]).to_not eq 0
+   expect(response[:results].count).to eq 1
+  end
 end
