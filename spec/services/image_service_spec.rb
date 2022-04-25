dc11f4b7300f14f5edd8d7fac77acf588f7217be
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-24 22:44:49 -0400

    change test to match stub

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -2,40 +2,49 @@ require 'rails_helper'
 
 RSpec.describe ImageService, type: :service do
 
+  before do
+    base = 'https://api.unsplash.com/search/photos?'
+    key = "client_id=#{ENV['unsplash_api']}&"
+    options = 'order_by=relevant&page=1&per_page=1&'
+    query = "query=chicago, il"
+
+    @expected = [base, key, options, query].join
+  end
+
   it 'builds a url' do
     base = 'https://api.unsplash.com/search/photos?'
     key = "client_id=#{ENV['unsplash_api']}&"
     options = 'order_by=relevant&page=1&per_page=1&'
-    query = "query=atlanta, ga"
+    query = "query=chicago, il"
 
     expected = [base, key, options, query].join
-    expect(ImageService.build_url('atlanta, ga')).to eq expected
+    expect(ImageService.build_url('chicago, il')).to eq expected
   end
 
   it "recieves a 200 response" do
     base = 'https://api.unsplash.com/search/photos?'
     key = "client_id=#{ENV['unsplash_api']}&"
     options = 'order_by=relevant&page=1&per_page=1&'
-    query = "query=atlanta, ga"
+    query = "query=chicago, il"
 
     url = [base, key, options, query].join
 
-    response = ImageService.send_request(url)
+    response = File.read('spec/fixtures/images/good_response.json')
 
+    stub_request(:get, url).to_return(status: 200, body: response)
     expect(response.status).to eq 200
   end
 
   it "returns the data for one image" do
-    response = ImageService.get_image('atlanta, ga')
+  response = ImageService.get_image('chicago, il')
 
    expect(response[:total]).to_not eq 0
    expect(response[:results].count).to eq 1
 
    expected = response[:results][0]
 
-
-   expect(expected[:urls][:full]).to eq("https://images.unsplash.com/photo-1599697985689-7fad9fd0c94c?crop=entropy&cs=srgb&fm=jpg&ixid=MnwzMjI0MzN8MHwxfHNlYXJjaHwxfHxhdGxhbnRhJTJDJTIwZ2F8ZW58MHx8fHwxNjUwODUyOTI0&ixlib=rb-1.2.1&q=85")
-   expect(expected[:alt_description]).to eq("woman in gray cardigan with silver necklace")
+   expect(expected[:urls][:full]).to eq("https://images.unsplash.com/photo-1602276506752-cec706667215?crop=entropy&cs=srgb&fm=jpg&ixid=MnwzMjI0MzN8MHwxfHNlYXJjaHwxfHxjaGljYWdvJTJDJTIwaWx8ZW58MHx8fHwxNjUwODU0NjM3&ixlib=rb-1.2.1&q=85")
+   expect(expected[:alt_description]).to eq("white concrete building during daytime")
 
    credits = expected[:user]
 
