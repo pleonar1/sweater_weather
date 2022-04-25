3d830dcc6b52260232788372d7ff3b71eba3fb63
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-25 12:21:52 -0400

    Build and test yelp service

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,17 +1,21 @@
 class YelpService < ApplicationService
   class << self
     def connection
-      Faraday.new('https://api.yelp.com/v3/businesses/')
+      Faraday.new(url: 'https://api.yelp.com/v3/') do |faraday|
+        faraday.headers['Authorization'] = ENV['yelp_api']
+      end
     end
 
     def parse_json(response)
       JSON.parse(response.body, symbolize_names: true)
     end
 
-    def get_yelp(location)
-      response = connection.get("search?#{location}") do |faraday|
-        faraday.params['Authorization'] = ENV['yelp_api']
+    def get_yelp(food_type, location)
+      response = connection.get('businesses/search') do |faraday|
+        faraday.params['term'] = food_type
+        faraday.params['location'] = location
       end
+      parse_json(response)
     end
   end
 end
--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe YelpService, type: :service do
 
 
-  it "gets a response" do
-    response = YelpService.get_yelp('denver')
+  it "gets a response that returns the city, the name and address of the restaurant" do
+    response = YelpService.get_yelp('chinese', 'denver')
 
-    expect(response).to be_successful
+
+    expect(response[:businesses][0][:name]).to eq("Taste of China")
+    expect(response[:businesses][0][:coordinates]).to eq({:latitude=>39.74702, :longitude=>-105.02571})
+    expect(response[:businesses][0][:location][:address1]).to eq("1935 Federal Blvd")
+    expect(response[:businesses][0][:location][:city]).to eq("Denver")
+    expect(response[:businesses][0][:location][:zip_code]).to eq("80204")
+
   end
 end
