6c72bc140bf11a224f12f1e25dfc6e0ef004fa0f
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-25 03:30:42 -0400

    Go back to hand rolled routes.  add forcast controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :forecast, only: [:show]
-      resources :images, only: [:show]
+      # resources :forecast, only: [:show]
+      # resources :images, only: [:show]
     end
   end
 
-   # get 'api/v1/images', to: 'images#show'
+   get 'api/v1/images', to: 'images#show'
+   get 'api/v1/forcasts', to: 'forecasts#show'
 end
