f6a1bc0437846b2f36b55f27ca58d3f8fdaebb43
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-26 20:24:32 -0400

    Start user request spec

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Create User', type: :request do
+  it 'creates a user and posts to database' do
+    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
+    params = {
+      "email": "paul@email.com",
+      "password": "realpassword",
+      "password_confirmation": "realpassword"
+    }
 
+    # post 'api/v1/users'
+  end
 
 
 
