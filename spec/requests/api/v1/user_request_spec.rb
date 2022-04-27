c2d2e967b1be7379744d9301059f9b910727fb21
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-26 20:45:18 -0400

    Add faker to gem file

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -1,41 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe 'Create User', type: :request do
- #
- #  before :each do
- #   # corrent input
- #   @user_params_1 = {
- #     email: Faker::Internet.email,
- #     password: 'newpassword',
- #     password_confirmation: 'newpassword'
- #   }.to_json
- #
- #   # passwords not matching
- #   @user_params_2 = {
- #     email: Faker::Internet.email,
- #     password: 'password',
- #     password_confirmation: 'paSS'
- #   }.to_json
- #
- #   # email not given
- #   @user_params_3 = {
- #     email: '',
- #     password: 'password',
- #     password_confirmation: 'password'
- #   }.to_json
- #
- #   # not json
- #   @user_params_4 = {
- #     email: Faker::Internet.email,
- #     password: 'password',
- #     password_confirmation: 'password'
- #   }
- #
- #   @headers = {
- #     'CONTENT_TYPE' => 'application/json',
- #     'ACCEPT' => 'application/json'
- #   }
- # end
+
+  before :each do
+   # corrent input
+   @user_params_1 = {
+     email: Faker::Internet.email,
+     password: 'newpassword',
+     password_confirmation: 'newpassword'
+   }.to_json
+
+   # passwords not matching
+   @user_params_2 = {
+     email: Faker::Internet.email,
+     password: 'password',
+     password_confirmation: 'paSS'
+   }.to_json
+
+   # email not given
+   @user_params_3 = {
+     email: '',
+     password: 'password',
+     password_confirmation: 'password'
+   }.to_json
+
+   # not json
+   @user_params_4 = {
+     email: Faker::Internet.email,
+     password: 'password',
+     password_confirmation: 'password'
+   }
+
+   @headers = {
+     'CONTENT_TYPE' => 'application/json',
+     'ACCEPT' => 'application/json'
+   }
+ end
   xit 'creates a user and posts to database' do
     headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
     params = {
