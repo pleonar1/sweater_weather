e90dc3471961df49a7259fee2d22010a56d8bec2
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-26 22:03:45 -0400

    take out pry

--- spec/requests/api/v1/sessions_request_sepc.rb
+++ spec/requests/api/v1/sessions_request_sepc.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'session request', type: :request do
 
-  # it "returns json in correct format" do
-  #   data = {
-  #    'email': 'paul@gmail.com',
-  #    'password': "password",
-  #    'password_confirmation': 'password'
-  #          }
-  #
-  #  post '/api/v1/users', params: data
-  #
-  #  login_data = {
-  #    'email': 'paul@gmail.com',
-  #    'password': 'password'
-  #               }
-  #
-  #   post '/api/v1/sessions', params: login_data
-  # end
+  it "returns json in correct format" do
+    data = {
+     'email': 'paul@gmail.com',
+     'password': "password",
+     'password_confirmation': 'password'
+           }
+
+   post '/api/v1/users', params: data
+
+   login_data = {
+     'email': 'paul@gmail.com',
+     'password': 'password'
+                }
+
+    # post '/api/v1/sessions', params: login_data
+  end
 
 end
