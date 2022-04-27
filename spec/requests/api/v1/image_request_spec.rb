3272740827b1e8a8ec235e17f4424fdad4c41df3
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-27 06:33:21 -0400

    Fix testing for image request

--- spec/requests/api/v1/image_request_spec.rb
+++ spec/requests/api/v1/image_request_spec.rb
@@ -7,12 +7,40 @@ RSpec.describe 'Image Request', type: :request do
     stub_request(:get, url).to_return(status: 200, body: image_response)
   end
 
-  it "returns a sucessful response" do
+  it "returns a sucessful response in the correct format" do
     get '/api/v1/images?location=chicago, il'
 
-    expect(response).to be_successful
     expect(response.status).to eq 200
 
     result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(result).to be_a Hash
+    expect(result[:data]).to include(:type)
+    expect(result[:data]).to include(:image)
+    expect(result[:data]).to include(:credits)
+
+    expect(result[:data][:type]).to be_a String
+    expect(result[:data][:type]).to eq("image")
+
+    expect(result[:data][:image]).to be_a Hash
+    expect(result[:data][:image]).to include(:url)
+    expect(result[:data][:image][:url]).to be_a String
+    expect(result[:data][:image]).to include(:description)
+    expect(result[:data][:image][:description]).to be_a String
+
+    expect(result[:data][:credits]).to be_a Hash
+    expect(result[:data][:credits]).to include(:source_name)
+    expect(result[:data][:credits]).to include(:source_url)
+    expect(result[:data][:credits]).to include(:source_photo)
+    expect(result[:data][:credits]).to include(:location)
+
+    expect(result[:data][:credits][:source_name]).to be_a String
+    expect(result[:data][:credits][:source_name]).to eq("Dylan LaPierre")
+    expect(result[:data][:credits][:source_url]).to be_a String
+    expect(result[:data][:credits][:source_url]).to eq("https://unsplash.com/@drench777")
+    expect(result[:data][:credits][:source_photo]).to be_a String
+    expect(result[:data][:credits][:source_photo]).to eq("https://unsplash.com/blog/content/images/max/2560/1-VnKoValwGK3-d1bZhD6sVA.jpeg")
+    expect(result[:data][:credits][:location]).to be_a String
+    expect(result[:data][:credits][:location]).to eq("Chicago ")
   end
 end
