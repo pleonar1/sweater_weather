15c5f7c8683da78d9c31b53d07b0af3d4d1e7cf0
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-26 20:56:51 -0400

    Pass test then it failed

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -36,15 +36,21 @@ RSpec.describe 'Create User', type: :request do
      'ACCEPT' => 'application/json'
    }
  end
-  xit 'creates a user and posts to database' do
-    headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json' }
-    params = {
-      "email": "paul@email.com",
-      "password": "realpassword",
-      "password_confirmation": "realpassword"
-    }
+  it 'creates a user and posts to database' do
 
-    post '/api/v1/users', headers: headers, params: params
+    post '/api/v1/users', headers: @headers, params: @user_params_1
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    user_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(user_json).to have_key(:data)
+    expect(user_json[:data]).to have_key(:type)
+    expect(user_json[:data]).to have_key(:id)
+    expect(user_json[:data]).to have_key(:attributes)
+    expect(user_json[:data][:attributes]).to have_key(:email)
+    expect(user_json[:data][:attributes]).to have_key(:api_key)
   end
 
 
