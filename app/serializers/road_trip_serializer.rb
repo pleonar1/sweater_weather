423c35d242e1c4ce49f582aa3c37e352aacf1330
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-27 02:07:13 -0400

    Add Road trip serializer

---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,36 @@
+class RoadTripSerializer
+
+  def self.format_data(road_trip)
+    {
+      "data": {
+        "id": "null",
+        "type": "roadtrip",
+        "attributes": {
+          "start_city": road_trip.start_city,
+          "end_city": road_trip.end_city,
+          "travel_time": road_trip.travel_time,
+          "weather_at_eta": {
+            "temperature": road_trip.end_temp,
+            "conditions": road_trip.end_conditions
+          }
+        }
+      }
+    }
+  end
+
+  def self.no_route(start, destination)
+     {
+       "data": {
+         "id": "null",
+         "type": "roadtrip",
+         "attributes": {
+           "start_city": start,
+           "end_city": finish,
+           "travel_time": "impossible",
+           "weather_at_eta": "null"{
+           }
+         }
+       }
+     }
+  end
+end
--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'road trip request', type: :request do
+  before do
+    @user = User.create(email: "paullll@gmail.com", password: "password", password_confirmation: "password")
 
+    @headers = { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
+
+    # @request_body = {
+    #       :or
+    # }
+  end
 
 
 
