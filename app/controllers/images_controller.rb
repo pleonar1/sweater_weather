6eb74877cc26312f3e136edfd91fbb6e7e7a460d
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-27 06:34:58 -0400

    add message to invalid location

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,7 +3,7 @@ class ImagesController < ApplicationController
   def show
     response = ImageFacade.get_image(params[:location])
     if response[:results] == []
-      render json: response, status: 404
+      render json: { data: { message: 'no results'}}, status: 404
     else
       render json: response, status: 200
     end
