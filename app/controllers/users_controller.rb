b76d1830708cd363817ef530116b7b88b264d6aa
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-27 08:25:55 -0400

    take out unnecessary line

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
     if user == nil || user.email == ''
       render json: { data: { message: 'Invalid Email or Password'}}, status: 404
     elsif params[:password] == params[:password_confirmation]
-      user.save
       render json: UserSerializer.user_response(user), status: 201
     else
       render json: { data: { message: 'Invalid Email or Password'}}, status: 404
