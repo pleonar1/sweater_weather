5111e6195b4f4f6906b0c73e55291acd1d631996
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-27 12:10:56 -0400

    last edge case

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email])
-    if user != nil
-      user = User.find_by(email: params[:session][:email])
-      user.authenticate(params[:password])
-      render json: UserSerializer.user_response(user), status: 200
-    elsif user == nil
+    if user.present?
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        render json: UserSerializer.user_response(user), status: 200
+      else
+        render json: { data: { message: 'Invalid Email or Password'}}, status: 404
+      end
+    else
       render json: { data: { message: 'Invalid Email or Password'}}, status: 404
     end
   end
