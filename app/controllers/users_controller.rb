2f5f07984e7e0a011bab69bc58ebf6402df09a1d
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-27 06:02:53 -0400

    Finish edge case testing for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 
   def create
-    if params[:password] == params[:password_confirmation]
+    user = User.create(user_params)
+    if user == nil || user.email == ''
+      render json: { data: { message: 'Invalid Email or Password'}}, status: 404
+    elsif params[:password] == params[:password_confirmation]
       user = User.create(user_params)
       render json: UserSerializer.user_response(user), status: 201
     else
-      render status: 404
-      render json: { data: { message: 'Invalid Email or Password'}}
+      render json: { data: { message: 'Invalid Email or Password'}}, status: 404
     end
   end
 
