35192e5e7fde7e61ab35da420538b9d2a8d703e0
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-27 01:54:25 -0400

    create road trip request spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:session][:email])
     if user != nil
-      user = User.find_by(email: params[:email])
+      user = User.find_by(email: params[:session][:email])
       user.authenticate(params[:password])
       render json: UserSerializer.user_response(user), status: 200
     elsif user == nil
---
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'road trip request', type: :request do
+
+
+
+
+
+
+
+
+end
