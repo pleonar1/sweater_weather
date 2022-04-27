4497440230969f4c67deb47da83784c80c21684c
Paul Leonard <paulpaulleonard@gmail.com>
2022-04-26 22:53:06 -0400

    Green lights!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,14 @@
 class SessionsController < ApplicationController
 
   def create
-
+    user = User.find_by(email: params[:email])
+    if user != nil
+      user = User.find_by(email: params[:email])
+      user.authenticate(params[:password])
+      render json: UserSerializer.user_response(user), status: 200
+    elsif user == nil
+      render json: { data: { message: 'Invalid Email or Password'}}
+    end
   end
 
 
--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -3,23 +3,52 @@ require 'rails_helper'
 RSpec.describe 'session request', type: :request do
 
   it "returns json in correct format" do
-    data = {
-     'email': 'paul@gmail.com',
-     'password': "password",
-     'password_confirmation': 'password'
-           }
+    headers = {
+      'CONTENT_TYPE' => 'application/json',
+      'ACCEPT' => 'application/json'
+              }
+    user_params_1 = {
+      email: "paul@gmail.com",
+      password: 'newpassword',
+      password_confirmation: 'newpassword'
+    }.to_json
 
-   post '/api/v1/users', params: data
+   post '/api/v1/users', headers: headers, params: user_params_1
 
    login_data = {
-     'email': 'paul@gmail.com',
-     'password': 'password'
-                }
+     email: 'paul@gmail.com',
+     password: 'newpassword'
+   }.to_json
 
-    post '/api/v1/sessions', params: login_data
+    post '/api/v1/sessions', headers: headers, params: login_data
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
   end
+
+  it "if improper credentials, returns a invalid response" do
+    headers = {
+      'CONTENT_TYPE' => 'application/json',
+      'ACCEPT' => 'application/json'
+              }
+    user_params_1 = {
+      email: "paul@gmail.com",
+      password: 'newpassword',
+      password_confirmation: 'newpassword'
+    }.to_json
+
+   post '/api/v1/users', headers: headers, params: user_params_1
+
+   login_data = {
+     email: 'pau@gmail.com',
+     password: 'newpassword'
+   }.to_json
+
+    post '/api/v1/sessions', headers: headers, params: login_data
+
+    expect(response).to be_successful
+  end
+
+
 
 end
